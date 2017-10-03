3efe5055f27af9ed33f67373f1d218b16603d1c1
Adam Nelson <adamnelson@mines.edu>
2017-10-02 21:50:01 -0600

    Added resetLevel, draw, and advance/kill methods to Player

--- src/architecture/Player.java
+++ src/architecture/Player.java
@@ -17,4 +17,51 @@ public class Player implements Drawable {
 	}
 	private PlayerStatus playerStatus;
 	
+	public Player(int location) {
+		//Constructor calls method to reset player stats each level
+		resetLevel(location);
+	}
+	
+	public void resetLevel(int Location) {
+		//Called every level to reset player stats
+		playerStatus = PlayerStatus.OK;
+		this.location = location;
+		damagePoints = 0;
+		advancePoints = 0;
+	}
+
+	//Drawable Method
+	public void draw() {
+		System.out.print('P');
+	}
+	
+	public boolean canAdvance() {
+		if (advancePoints >= POINTS_TO_ADVANCE || playerStatus == PlayerStatus.ADVANCING) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isDead() {
+		return (playerStatus == PlayerStatus.DEAD || damagePoints >= POINTS_TO_DIE);
+	}
+	
+	public void addPoint() {
+		advancePoints++;
+	}
+	
+	public void takeDamage() {
+		damagePoints++;
+	}
+	
+	public void killed() {
+		playerStatus = PlayerStatus.DEAD;
+	}
+	
+	public void wonAdvance() {
+		playerStatus = PlayerStatus.ADVANCING;
+	}
+	
+	//The remaining methods pertain to changing the player's location
+	
 } //End of Class
