79edacdb5e58e4f394ff469bc7610e4cd94d0f59
Adam Nelson <adamnelson@mines.edu>
2017-10-02 21:26:56 -0600

    Completed Drawable interface

--- src/architecture/Drawable.java
+++ src/architecture/Drawable.java
@@ -1,5 +1,9 @@
 package architecture;
 
+/*
+ * All game pieces may be "drawn" - just printed on console, 
+ * no parameters needed.
+ */
 public interface Drawable {
-
+	public void draw();
 } //End of Interface
