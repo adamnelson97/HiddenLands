ef69a1daa13838575de9f2dbabd7850ebb6256e1
Adam Nelson <adamnelson@mines.edu>
2017-10-02 21:37:08 -0600

    Added attributes to Player class, including enum PlayerStatus

--- src/architecture/Player.java
+++ src/architecture/Player.java
@@ -4,6 +4,17 @@ package architecture;
  * This class contains all data and methods relative to the player,
  * such as health, status, and location.
  */
-public class Player {
+public class Player implements Drawable {
 
+	public static final int POINTS_TO_ADVANCE = 3;
+	public static final int POINTS_TO_DIE = 3;
+	
+	private int location;
+	private int advancePoints;
+	private int damagePoints;
+	public enum PlayerStatus {
+		DEAD, ADVANCING, OK;
+	}
+	private PlayerStatus playerStatus;
+	
 } //End of Class
