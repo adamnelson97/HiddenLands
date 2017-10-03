6b7abebca204a7c3fd232dad8ce90d797cd20a2d
Adam Nelson <adamnelson@mines.edu>
2017-10-02 23:01:29 -0600

    Added setupLevel and displayBoard methods to GameEngine

--- src/architecture/GameEngine.java
+++ src/architecture/GameEngine.java
@@ -2,6 +2,8 @@ package architecture;
 
 import java.util.ArrayList;
 
+import pieces.GamePiece;
+
 /*
  * This class contains the main method, initiates LevelEngine,
  * contains the method to display the board to the console,
@@ -14,8 +16,8 @@ public class GameEngine {
 	 * The game board is a square with an odd number of spaces
 	 * so there is an exact center.
 	 */
-	public static final int BOARD_SIZE = 21;
-	
+	public static final int BOARD_SIZE = 15;
+
 	/*
 	 * The number of levels for the game are determined by the difficulty.
 	 * There are a minimum of two levels, which are hard coded into LevelEngine.
@@ -34,9 +36,41 @@ public class GameEngine {
 	private ArrayList<GamePiece> interactingPieces;
 	//GameEngine has one instance of Player
 	private Player player;
-	
-	
-	
+
+	//Constructor
+	public GameEngine() {
+		levels = new LevelEngine();
+	}
+
+	//Methods
+	public void setupLevel(int levelNum) {
+		// LevelEngine needs to create the specified level
+		levels.createLevel(levelNum);
+		// request data structures from LevelEngine
+		pieces = levels.getPieces();
+		movingPieces = levels.getMovingPieces();
+		interactingPieces = levels.getInteractingPieces();
+		// reset player statistics, starting location determined
+		// by level engine
+		player.resetLevel(levels.getPlayerStartLoc());
+	}
+
+	public void displayBoard() {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			for (int i = 0; i < BOARD_SIZE; i++) {
+				// Ensure player is always drawn
+				if (i == player.getLocation()) 
+					player.draw();
+				else if (pieces[i] == null)
+					System.out.print(' ');
+				else
+					pieces[i].draw();
+				System.out.print('|');
+			}
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
