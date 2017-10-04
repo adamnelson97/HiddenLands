dc214ccadd65831e82e0290c4015e6919d7563e9
Adam Nelson <adamnelson@mines.edu>
2017-10-04 13:39:03 -0600

    Man class now updates player on their current damage/prizes status

--- src/pieces/Man.java
+++ src/pieces/Man.java
@@ -29,6 +29,7 @@ public class Man extends GamePiece {
 			System.out.println("\nYou encounter a Man! You ask him some questions to aid you in your quest.");
 			System.out.println("Man: 'You can take a maximum of " + Player.POINTS_TO_DIE + " hits before you die.'");
 			System.out.println("     'You need " + Player.POINTS_TO_ADVANCE + " prizes to move on to the next level.");
+			System.out.println("     'You have currently taken " + player.getDamagePoints() + " hits and have " + player.getAdvancePoints() + " prizes.");
 		}
 		return InteractionResult.NONE;
 	}
