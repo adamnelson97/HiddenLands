65e5e7bb80b467bf9721c23b802e8969db082a05
Adam Nelson <adamnelson@mines.edu>
2017-10-04 11:54:30 -0600

    Completed Man piece class.

--- src/pieces/Man.java
+++ src/pieces/Man.java
@@ -4,18 +4,31 @@ import java.awt.Point;
 
 import architecture.Drawable;
 import architecture.InteractionResult;
+import architecture.Player;
 
 public class Man extends GamePiece {
 
-	//TODO: write Man Class
-	
+	/*
+	 * Symbol: 'M'
+	 * 
+	 * Motion: None
+	 * 
+	 * Interaction: NONE. The man tells the player the number of hits they can take,
+	 * and the number of points they need to advance. Has a 1/3 chance of appearing somewhere on the board.
+	 */
+
 	public Man(char symbol, Point location) {
 		super(symbol, location);
 	}
 
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Point playerLocation) {
-		return null;
+		if (playerLocation == getLocation()) {
+			System.out.println("\nYou encounter a Man! You ask him some questions to aid you in your quest.");
+			System.out.println("Man: 'You can take a maximum of " + Player.POINTS_TO_DIE + " hits before you die.'");
+			System.out.println("     'You need " + Player.POINTS_TO_ADVANCE + " prizes to move on to the next level.");
+		}
+		return InteractionResult.NONE;
 	}
 
-}
+} //End of Class
