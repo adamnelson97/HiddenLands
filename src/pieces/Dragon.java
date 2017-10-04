b2a5d52b5d0facae8dfd05750e9ced2a7d155a1e
Adam Nelson <adamnelson@mines.edu>
2017-10-03 21:40:57 -0600

    Completed Dragon piece class

--- src/pieces/Dragon.java
+++ src/pieces/Dragon.java
@@ -8,21 +8,31 @@ import architecture.InteractionResult;
 public class Dragon extends GamePiece {
 
 	/*
-	 * Symbol: 'F'. Dwarves use 'D' so Dragons will use 'F' for 'Fire'
+	 * Symbol: 'F'. Uses 'F' for 'Fire' since Dwarves use 'D'.
 	 * 
 	 * Motion: None
 	 * 
-	 * Interaction: NONE. Dragon is 'sleeping', so it only implements Drawable.
+	 * Interaction: KILL. Dragon is initially sleeping, and gives warning. If the player lands on that space again they die
 	 */
-	
+
+	int counter;
+
 	public Dragon(char symbol, Point location) {
 		super(symbol, location);
+		counter = 0;
 	}
 
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Point playerLocation) {
-		System.out.println("\nA sleeping dragon lays ahead! Tread lightly, adventurer.\n");
-		return null;
+		if (counter == 0) {
+			System.out.println("\nA sleeping dragon lays ahead! Tread lightly, adventurer.\n");
+			counter++; //Increases counter so the next encounter the player gets a different interaction
+			return InteractionResult.NONE;
+		}
+		else {
+			System.out.println("You have awoken the dragon! He sets you ablaze with his flames!");
+			return InteractionResult.KILL;
+		}
 	}
-
-}
+	
+} //End of Class
