5df4fc3dc70c25d543ba6666582a599c6d062979
Adam Nelson <adamnelson@mines.edu>
2017-10-04 12:21:12 -0600

    Completed Wizard class

--- src/pieces/Wizard.java
+++ src/pieces/Wizard.java
@@ -7,16 +7,36 @@ import architecture.InteractionResult;
 
 public class Wizard extends GamePiece {
 
-	
-	//TODO: write Wizard class
-	
+
+	/* 
+	 * Symbol: 'W'. Used to confuse player as it is the same as the Witch.
+	 * 
+	 * Motion: None
+	 * 
+	 * Interaction: HIT. Has range of +/- 2 spaces from current space,
+	 * i.e. if the Wizard is on space 3, he can hit a target on spaces 1-5.
+	 */
+
+	//Constructor
 	public Wizard(char symbol, Point location) {
 		super(symbol, location);
 	}
 
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Point playerLocation) {
-		return null;
+		double playerX = playerLocation.getX();
+		double playerY = playerLocation.getY();
+		double wizX = getLocation().getX();
+		double wizY = getLocation().getY();
+		double hitDistance = 1;
+		
+		if (playerX <= wizX + hitDistance && playerX  >= wizX - hitDistance && playerY <= wizY + hitDistance && playerY >= wizY - hitDistance) {
+			System.out.println("\nYou encounter a Wizard! His spells play tricks on you, and you injure yourself escaping.");
+			return InteractionResult.HIT;
+		}
+		else {
+			return InteractionResult.NONE;
+		}
 	}
 
 }
