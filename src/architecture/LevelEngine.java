c20516ba56da6966a090483545190ef30f711441
Adam Nelson <adamnelson@mines.edu>
2017-10-03 20:44:11 -0600

    Copied legacy code from old test classes. Will need to update tests for 2D movement.

--- src/architecture/LevelEngine.java
+++ src/architecture/LevelEngine.java
@@ -1,5 +1,7 @@
 package architecture;
 
+import java.util.ArrayList;
+
 /*
  * This class creates the data structures to store the various game pieces,
  * contains methods with hard-coded level data, as well as methods to create
@@ -7,6 +9,12 @@ package architecture;
  */
 
 public class LevelEngine {
+	
+	//Attributes
+	//The board is stored as a 2D array of pieces
+	private Drawable[][] levelPieces;
+	private ArrayList<Moveable> movingPieces;
+	private ArrayList<GamePiece> interactringPieces;
 	//TODO: write createLevel
 	//TODO: write getPieces
 	//TODO: write getMovingPieces
