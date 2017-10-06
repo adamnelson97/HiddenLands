b26fe47b4906a836f13ecc5851a0c2a39e2c1597
Adam Nelson <[adamnelson@mines.edu]>
2017-10-06 11:53:22 -0600

    Added Orc and Unicorn Classes

---
+++ src/pieces/Orc.java
@@ -0,0 +1,25 @@
+package pieces;
+
+import java.awt.Point;
+
+import architecture.Drawable;
+import architecture.InteractionResult;
+import architecture.Player;
+
+public class Orc extends GamePiece {
+
+	
+	//TODO: Complete Orc Class
+	
+	public Orc(char symbol, Point location) {
+		super(symbol, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[][] pieces, Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/pieces/Unicorn.java
@@ -0,0 +1,24 @@
+package pieces;
+
+import java.awt.Point;
+
+import architecture.Drawable;
+import architecture.InteractionResult;
+import architecture.Player;
+
+public class Unicorn extends GamePiece {
+
+	
+	//TODO: Complete Unicorn class
+	public Unicorn(char symbol, Point location) {
+		super(symbol, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[][] pieces, Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
