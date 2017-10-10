d104ed3dc45e55c17981c8c3a07a7cd8d98dc968
Adam Nelson <adamnelson@mines.edu>
2017-10-10 00:09:49 -0600

    Edited in-game text in GameEngine and LevelEngine

--- src/architecture/GameEngine.java
+++ src/architecture/GameEngine.java
@@ -188,40 +188,60 @@ public class GameEngine {
 			System.out.println("Congratulations, " + player.getName() + ", you won!");
 			switch(numLevels) {
 			case 1:
-				System.out.println("You have made sufficient progress on your journey. You have faced formidable\n"
-						+ "monsters, but greater threats lay ahead. Play again on Medium to further your quest and\n"
-						+ "save the villagers! ");
+				System.out.println("You have made sufficient progress on your journey. You have faced");
+				System.out.println("formidable monsters, but greater threats lay ahead. Play again on");
+				System.out.println("Medium to further your quest and save the villagers! ");
+				System.out.println();
 				break;
 			case 2:
-				System.out.println("You have made admirable progress along your journey. Word has begun to spread\n"
-						+ "that a warrior is going to brave the beast. Strengthen yourself, and play again on Hard to\n"
-						+ "complete your conquest!");
+				System.out.println("You have made admirable progress along your journey. Word has begun");
+				System.out.println("to spread that a warrior is going to brave the beast. Strengthen");
+				System.out.println("yourself, and play again on Hard to complete your conquest!");
+				System.out.println();
 				break;
 			case 3:
-				System.out.println("You have slayed the mighty beast! Huzzah! You begin the long walk back to the\n"
-						+ "village to inform the people of the good news...\n"
-						+ "'...So you truly slayed it? You killed that evil monster?' a child asks you. You nod your\n"
-						+ "head in agreement. You step into the inn you first entered many moons ago. You notice the\n"
-						+ "old man, and his eyes widen. 'So it's true!' he exclaims. 'My boy, I never doubted you. But\n"
-						+ "word has come here that the great city of Glor Infitae is being laid waste to by an unknown foe.\n"
-						+ "Dark forces are surely at work. If you can, we beseech you to help them as you did us!'\n"
-						+ "\nYou consider the proposition, and reply 'I will help any good people, for good in this\n"
-						+ "world must be protected.'\n"
-						+ "\nPlay again on HARDCORE to face the greatest threat in all the land, the fiery rage of\n"
-						+ "THE SKELETON KING!");
+				System.out.println("You have slayed the mighty beast! Huzzah! You begin the long walk");
+				System.out.println("back to the village to inform the people of the good news...");
+				System.out.println();
+				System.out.println("'...So you truly slayed it? You killed that evil monster?' a child");
+				System.out.println("asks you. You nod your head in affirmation. You step into the inn you");
+				System.out.println("first entered many moons ago. You notice the old man, and his eyes widen.");
+				System.out.println();
+				System.out.println("'So it's true!' he exclaims. 'My boy, I never doubted you. But word has");
+				System.out.println("come here that the great city of Glor Infitae is being laid waste to by");
+				System.out.println("an unknown foe. Dark forces are surely at work. If you can, we beseech");
+				System.out.println("you to help them as you did us!'");
+				System.out.println();
+				System.out.println("You consider the proposition, and reply 'I will help any good people,");
+				System.out.println("for good in this world must be protected.'");
+				System.out.println();
+				System.out.println("Play again on HARDCORE to face the greatest threat in all the land,");
+				System.out.println("the fiery rage of THE SKELETON KING!");
+				System.out.println();
 				break;
 			case 4:
-				System.out.println("Word has spread of your incredible victory over the terrible Skeleton King! Villagers openly\n"
-						+ "cry out their thanks for you, offering meat, ale, horses, and gold as tokens of their gratitude.\n"
-						+ "A squire approaches you, and offers you a scroll. The scroll dictates that the Princess, caretaker\n"
-						+ "and ruler of Glor Infitae has summoned you for an audience. You mount your horse and stride through the\n"
-						+ "city, stopping before the marble pillars of the Royal Hall.\n"
-						+ "\nYou dismount, and the guards enter the giant oak doors into the throne room where the Princess sits,\n"
-						+ "beaming in light as if an angel. 'Come here, " + player.getName() + "', she says. You approach her and\n"
-						+ "kneel before her.\n'No one can question your heroics and selfless actions to defending our noble land.\n"
-						+ "It is for this that I now bestow upon you the Heart of Aetna, our guardian spirit, and declare this day\n"
-						+ "in your honor!' The hall erupts in racous applause as townspeople cheer your name!\n"
-						+ "\nThank you for playing. Please play again!");
+				System.out.println("Word has spread of your incredible victory over the terrible Skeleton King!");
+				System.out.println("Villagers openly cry out their thanks for you, offering meat, ale,");
+				System.out.println("horses, and gold as tokens of their gratitude. A squire approaches");
+				System.out.println("you, and offers you a scroll. The scroll dictates that the Princess");
+				System.out.println("and half elvish ruler of Glor Infitae Iselria Theris has summoned you");
+				System.out.println("for an audience. You mount your horse and stride through the city,");
+				System.out.println("stopping before the marble pillars of the Royal Hall.");
+				System.out.println();
+				System.out.println("You dismount, and the guards enter the giant oak doors into the throne");
+				System.out.println("room where the Princess sits, beaming in light as if an angel.");
+				System.out.println();
+				System.out.println("'Come here, " + player.getName() + "', she says. You approach her and");
+				System.out.println("kneel before her");
+				System.out.println("'No one can question your heroics and selfless actions to defending");
+				System.out.println("our noble land. It is for your nobility and chivalry that I now bestow");
+				System.out.println("upon you the Heart of Aetna, our guardian spirit, and declare this day");
+				System.out.println("in your honor!'");
+				System.out.println();
+				System.out.println("The hall erupts in racous applause as townspeople cheer your name!");
+				System.out.println();
+				System.out.println("Thank you for playing. Please play again!");
+				System.out.println();
 				break;
 			}
 		}
@@ -231,17 +251,27 @@ public class GameEngine {
 	public void intro() {
 		System.out.println("\n\n\n=====H I D D E N  L A N D S=====");
 		System.out.println("   ------by Adam Nelson------");
-		System.out.println("Welcome Adventurer! You are about to embark on a quest filled\n" +
-				"with danger, mysterious creatures, and treasure! Good luck!\n\n" + 
-				"Whilst traveling across the land, you see a village on the horizon.\n" + 
-				"Traveling to the village, you arrive at dusk. You enter the local inn, and order an ale.\n" +
-				"A wounded old man notices your sword, and approaches you.\n" +
-				"'Are you a warrior?' he asks. You nod your head in compliance.\n" +
-				"Maybe you can help us!' he exclaims. Every winter, we people of Corington are ravaged by a terrible\n" +
-				"monster, and have to rebuild our homes from his terrible destruction. We are too poor to move\n" +
-				"somewhere else, and our finest warriors have never returned. Please help us!\n" +
-				"\nHe offers you gold, but you turn it down, saying the honor of helping them is reward enough.\n" +
-				"What will you find on your journey? Glory? Love? Power? There is only one way to find out...\n");
+		System.out.println("Welcome Adventurer! You are about to embark on a quest filled");
+		System.out.println("with danger, mysterious creatures, and treasure! Good luck!");
+		System.out.println();
+		System.out.println("Whilst traveling across the land, you see a village on the horizon.");
+		System.out.println("Traveling to the village, you arrive at dusk. You enter the local");
+		System.out.println("inn, and order an ale.");
+		System.out.println("A wounded old man notices your sword, and approaches you.");
+		System.out.println();
+		System.out.println("'Are you a warrior?' he asks. You nod your head in compliance.");
+		System.out.println();
+		System.out.println("'Maybe you can help us!' he exclaims. 'Every winter, we people of");
+		System.out.println("Corington are ravaged by a terrible monster, and have to rebuild our");
+		System.out.println("homes from his terrible destruction. We are too poor to move somewhere");
+		System.out.println("else, and our finest warriors have never returned. Please help us!");
+		System.out.println();
+		System.out.println("He offers you gold, but you turn it down, saying the honor of helping");
+		System.out.println("them is reward enough.");
+		System.out.println();
+		System.out.println("What will you find on your journey? Glory? Love? Power? There is only");
+		System.out.println("one way to find out...");
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
--- src/architecture/LevelEngine.java
+++ src/architecture/LevelEngine.java
@@ -80,10 +80,11 @@ public class LevelEngine {
 	//TODO: Write levelOne()
 
 	public void levelOne() {
-		System.out.println("The old man says the monster has a lair in the nearby to the West\n"
-				+ " so you head to the Ehrlain Mountains, a region known for troublesome\n"
-				+ "conjurers of dark magik.");
-		
+		System.out.println("\nThe old man says the monster has a lair in the nearby to the West");
+		System.out.println("so you head to the Ehrlain Mountains, a region known for troublesome");
+		System.out.println("conjurers of dark magik.");
+		System.out.println();
+
 		levelPieces = new Drawable[GameEngine.BOARD_SIZE][GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<Moveable>();
 		interactingPieces = new ArrayList<GamePiece>();
@@ -91,6 +92,7 @@ public class LevelEngine {
 
 		//Pieces Go Here
 		levelPieces[5][0] = new Rock('@', new Point(5,0));
+		levelPieces[4][1] = new Rock('@', new Point(4,1));
 		levelPieces[5][1] = new Knight('K', new Point(5,1));
 
 		//Identify pieces that interact
@@ -101,7 +103,7 @@ public class LevelEngine {
 				}
 			}
 		}
-		
+
 		//Identify pieces that move
 		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
 			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
@@ -116,113 +118,138 @@ public class LevelEngine {
 	//TODO: Write levelTwo();
 
 	public void levelTwo() {
-		System.out.println("You have made sufficient progress on your journey. You have faced formidable\n"
-				+ "monsters, but greater threats lay ahead.\n"
-				+ "\nOut of the foothills, you enter the Srulmvic Timberland, a dense, dark forest covering the Mountains.\n"
-				+ "This land is known for harboring dangerous hordes of foul beings. Beware!");
+		System.out.println("You have made sufficient progress on your journey. You have faced");
+		System.out.println("formidable monsters, but greater threats lay ahead.");
+		System.out.println();
+		System.out.println("Out of the foothills, you enter the Srulmvic Timberland, a dense,");
+		System.out.println("dark forest covering the Mountains. This land is known for harboring");
+		System.out.println("dangerous hordes of foul beings. Beware!");
+		System.out.println();
 
 		levelPieces = new Drawable[GameEngine.BOARD_SIZE][GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<Moveable>();
 		interactingPieces = new ArrayList<GamePiece>();
 		startingLocation = new Point(7, 7);
-		
+
 		//Pieces Go Here
+		levelPieces[5][0] = new Rock('@', new Point(5,0));
+		levelPieces[4][1] = new Rock('@', new Point(4,1));
+		levelPieces[5][1] = new Knight('K', new Point(5,1));
 
 		//Identify pieces that interact
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof GamePiece) {
-							interactingPieces.add((GamePiece) levelPieces[i][j]);
-						}
-					}
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof GamePiece) {
+					interactingPieces.add((GamePiece) levelPieces[i][j]);
 				}
-				
-				//Identify pieces that move
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof Moveable) {
-							movingPieces.add((Moveable) levelPieces[i][j]);
-						}
-					}
+			}
+		}
+
+		//Identify pieces that move
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof Moveable) {
+					movingPieces.add((Moveable) levelPieces[i][j]);
 				}
+			}
+		}
 	}
 
 	//TODO: Write levelThree();
-	
+
 	public void levelThree() {
-		System.out.println("You have made admirable progress along your journey. Word has begun to spread\n"
-				+ "that a warrior is going to brave the beast. You can see the peak of Alindor,\n"
-				+ "the tallest mountain, up ahead. You spot a small cave opening nearby. That must be\n"
-				+ "the monster's lair...");
-		
+		System.out.println("You have made admirable progress along your journey. Word has begun");
+		System.out.println("to spread that a warrior is going to brave the beast. You can see");
+		System.out.println("the peak of Alindor, the tallest mountain glittering with the light");
+		System.out.println("of a thousand stars, up ahead. You spot a small cave opening nearby.");
+		System.out.println("That must be the monster's lair...");
+		System.out.println();
+
 		levelPieces = new Drawable[GameEngine.BOARD_SIZE][GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<Moveable>();
 		interactingPieces = new ArrayList<GamePiece>();
 		startingLocation = new Point(7, 7);
-		
+
 		//Pieces Go Here
+		levelPieces[5][0] = new Rock('@', new Point(5,0));
+		levelPieces[4][1] = new Rock('@', new Point(4,1));
+		levelPieces[5][1] = new Knight('K', new Point(5,1));
 
 		//Identify pieces that interact
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof GamePiece) {
-							interactingPieces.add((GamePiece) levelPieces[i][j]);
-						}
-					}
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof GamePiece) {
+					interactingPieces.add((GamePiece) levelPieces[i][j]);
 				}
-				
-				//Identify pieces that move
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof Moveable) {
-							movingPieces.add((Moveable) levelPieces[i][j]);
-						}
-					}
+			}
+		}
+
+		//Identify pieces that move
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof Moveable) {
+					movingPieces.add((Moveable) levelPieces[i][j]);
 				}
+			}
+		}
 	}
-	
+
 	//TODO: Write levelFour();
-	
+
 	public void levelFour() {
-		System.out.println("You have slayed the mighty beast! Huzzah! You begin the long walk back to the\n"
-				+ "village to inform the people of the good news...\n"
-				+ "'...So you truly slayed it? You killed that evil monster?' a child asks you. You nod your\n"
-				+ "head in agreement. You step into the inn you first entered many moons ago. You notice the\n"
-				+ "old man, and his eyes widen. 'So it's true!' he exclaims. 'My boy, I never doubted you. But\n"
-				+ "word has come here that the great city of Glor Infitae is being laid waste to by an unknown foe.\n"
-				+ "Dark forces are surely at work. If you can, we beseech you to help them as you did us!'\n"
-				+ "\nYou consider the proposition, and reply 'I will help any good people, for good in this\n"
-				+ "world must be protected.'\n"
-				+ "\nYou purchase a horse from a villager and ride into the night. You arrive at the city gates late, find\n"
-				+ "rest in a hostel before being awoken by the city guards. 'Are you the man who slew the monster of Alindor?'\n"
-				+ "You tell them your tale, and why you are here. They provide you with stronger armor, a sharper sword,\n"
-				+ "and a bow with which to face the Skeleton King. Rest well, Adventurer, for tomorrow you face him...");
-		
+		System.out.println("You have slayed the mighty beast! Huzzah! You begin the long walk");
+		System.out.println("back to the village to inform the people of the good news...");
+		System.out.println();
+		System.out.println("'...So you truly slayed it? You killed that evil monster?' a child");
+		System.out.println("asks you. You nod your head in affirmation. You step into the inn you");
+		System.out.println("first entered many moons ago. You notice the old man, and his eyes widen.");
+		System.out.println();
+		System.out.println("'So it's true!' he exclaims. 'My boy, I never doubted you. But word has");
+		System.out.println("come here that the great city of Glor Infitae is being laid waste to by");
+		System.out.println("an unknown foe. Dark forces are surely at work. If you can, we beseech");
+		System.out.println("you to help them as you did us!'");
+		System.out.println();
+		System.out.println("You consider the proposition, and reply 'I will help any good people,");
+		System.out.println("for good in this world must be protected.'");
+		System.out.println();
+		System.out.println("You purchase a horse from a villager and ride into the night. You arrive");
+		System.out.println("at the city gates late, find rest in a hostel before being awoken by the");
+		System.out.println("city guards.");
+		System.out.println();
+		System.out.println("'Are you the man who slew the monster of Alindor?' You tell them your tale,");
+		System.out.println("and why you are here. They provide you with stronger armor, a sharper sword,");
+		System.out.println("and a bow with which to face the Skeleton King! Rest well, Adventurer, for");
+		System.out.println("tomorrow your journey ends, one way or another...");
+		System.out.println();
+
 		levelPieces = new Drawable[GameEngine.BOARD_SIZE][GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<Moveable>();
 		interactingPieces = new ArrayList<GamePiece>();
 		startingLocation = new Point(7, 7);
-		
+
 		//Pieces Go Here
 		//TODO: Create Skeleton King boss
+		levelPieces[5][0] = new Rock('@', new Point(5,0));
+		levelPieces[4][1] = new Rock('@', new Point(4,1));
+		levelPieces[5][1] = new Knight('K', new Point(5,1));
 
 		//Identify pieces that interact
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof GamePiece) {
-							interactingPieces.add((GamePiece) levelPieces[i][j]);
-						}
-					}
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof GamePiece) {
+					interactingPieces.add((GamePiece) levelPieces[i][j]);
 				}
-				
-				//Identify pieces that move
-				for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
-					for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
-						if (levelPieces[i][j] instanceof Moveable) {
-							movingPieces.add((Moveable) levelPieces[i][j]);
-						}
-					}
+			}
+		}
+
+		//Identify pieces that move
+		for (int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			for (int j = 0; j < GameEngine.BOARD_SIZE; j++) {
+				if (levelPieces[i][j] instanceof Moveable) {
+					movingPieces.add((Moveable) levelPieces[i][j]);
 				}
+			}
+		}
 	}
 
 } //End of Class
