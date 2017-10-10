6029dec9c9a50c3694c1f13b8271c36089734512
Adam Nelson <adamnelson@mines.edu>
2017-10-10 00:15:23 -0600

    Edited in-game text in GameEngine and LevelEngine

--- src/architecture/LevelEngine.java
+++ src/architecture/LevelEngine.java
@@ -80,7 +80,7 @@ public class LevelEngine {
 	//TODO: Write levelOne()
 
 	public void levelOne() {
-		System.out.println("\nThe old man says the monster has a lair in the nearby to the West");
+		System.out.println("\n	The old man says the monster has a lair in the nearby to the West");
 		System.out.println("so you head to the Ehrlain Mountains, a region known for troublesome");
 		System.out.println("conjurers of dark magik.");
 		System.out.println();
@@ -118,10 +118,10 @@ public class LevelEngine {
 	//TODO: Write levelTwo();
 
 	public void levelTwo() {
-		System.out.println("You have made sufficient progress on your journey. You have faced");
+		System.out.println("	You have made sufficient progress on your journey. You have faced");
 		System.out.println("formidable monsters, but greater threats lay ahead.");
 		System.out.println();
-		System.out.println("Out of the foothills, you enter the Srulmvic Timberland, a dense,");
+		System.out.println("	Out of the foothills, you enter the Srulmvic Timberland, a dense,");
 		System.out.println("dark forest covering the Mountains. This land is known for harboring");
 		System.out.println("dangerous hordes of foul beings. Beware!");
 		System.out.println();
@@ -158,7 +158,7 @@ public class LevelEngine {
 	//TODO: Write levelThree();
 
 	public void levelThree() {
-		System.out.println("You have made admirable progress along your journey. Word has begun");
+		System.out.println("	You have made admirable progress along your journey. Word has begun");
 		System.out.println("to spread that a warrior is going to brave the beast. You can see");
 		System.out.println("the peak of Alindor, the tallest mountain glittering with the light");
 		System.out.println("of a thousand stars, up ahead. You spot a small cave opening nearby.");
@@ -197,26 +197,26 @@ public class LevelEngine {
 	//TODO: Write levelFour();
 
 	public void levelFour() {
-		System.out.println("You have slayed the mighty beast! Huzzah! You begin the long walk");
+		System.out.println("	You have slayed the mighty beast! Huzzah! You begin the long walk");
 		System.out.println("back to the village to inform the people of the good news...");
 		System.out.println();
-		System.out.println("'...So you truly slayed it? You killed that evil monster?' a child");
+		System.out.println("	'...So you truly slayed it? You killed that evil monster?' a child");
 		System.out.println("asks you. You nod your head in affirmation. You step into the inn you");
 		System.out.println("first entered many moons ago. You notice the old man, and his eyes widen.");
 		System.out.println();
-		System.out.println("'So it's true!' he exclaims. 'My boy, I never doubted you. But word has");
+		System.out.println("	'So it's true!' he exclaims. 'My boy, I never doubted you. But word has");
 		System.out.println("come here that the great city of Glor Infitae is being laid waste to by");
 		System.out.println("an unknown foe. Dark forces are surely at work. If you can, we beseech");
 		System.out.println("you to help them as you did us!'");
 		System.out.println();
-		System.out.println("You consider the proposition, and reply 'I will help any good people,");
+		System.out.println("	You consider the proposition, and reply 'I will help any good people,");
 		System.out.println("for good in this world must be protected.'");
 		System.out.println();
-		System.out.println("You purchase a horse from a villager and ride into the night. You arrive");
+		System.out.println("	You purchase a horse from a villager and ride into the night. You arrive");
 		System.out.println("at the city gates late, find rest in a hostel before being awoken by the");
 		System.out.println("city guards.");
 		System.out.println();
-		System.out.println("'Are you the man who slew the monster of Alindor?' You tell them your tale,");
+		System.out.println("	'Are you the man who slew the Orc of Alindor?' You tell them your tale,");
 		System.out.println("and why you are here. They provide you with stronger armor, a sharper sword,");
 		System.out.println("and a bow with which to face the Skeleton King! Rest well, Adventurer, for");
 		System.out.println("tomorrow your journey ends, one way or another...");
