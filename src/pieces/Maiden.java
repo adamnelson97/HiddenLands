1c8ed7173e7611a9c8a9237e13534e7aadd2de3e
Adam Nelson <adamnelson@mines.edu>
2017-10-18 20:04:02 -0600

    Changed output message from maiden interaction

--- src/pieces/Maiden.java
+++ src/pieces/Maiden.java
@@ -29,8 +29,8 @@ public class Maiden extends GamePiece {
 		Point playerLocation = player.getLocation();
 		if (playerLocation.getX() == getLocation().getX() && playerLocation.getY() == getLocation().getY()
 				&& !interacted) {
-			System.out.println("\nYou encounter a fair Maiden! She takes you to a cottage to rest,");
-			System.out.println("giving you bread, meat, and ale. You fall asleep...");
+			System.out.println("\nYou encounter a fair Maiden! She offers you bread, meat,");
+			System.out.println("and ale. You fall asleep...");
 			System.out.println("You awaken rejuvenated, but the maiden is gone...");
 			System.out.println("YOUR HEALTH HAS BEEN FULLY RESTORED.");
 			player.setDamagePoints(0);
