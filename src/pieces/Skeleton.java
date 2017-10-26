8ca515032f308c67136aa83056055c04917277f7
Adam Nelson <[adamnelson@mines.edu]>
2017-10-26 11:49:31 -0600

    Created framework for Skeleton class

---
+++ src/pieces/Skeleton.java
@@ -0,0 +1,29 @@
+package pieces;
+
+import java.awt.Point;
+
+import architecture.Drawable;
+import architecture.InteractionResult;
+import architecture.Moveable;
+import architecture.Player;
+
+public class Skeleton extends GamePiece implements Moveable {
+
+	public Skeleton(char symbol, Point location) {
+		super(symbol, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[][] pieces, Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move(Drawable[][] pieces, Point playerLocation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
