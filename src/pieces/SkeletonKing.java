9bc4f4892eaae2f9dc34b0ec846abe986672b03e
Adam Nelson <adamnelson@mines.edu>
2017-10-27 11:35:10 -0600

    Added SkeletonKing class.

---
+++ src/pieces/SkeletonKing.java
@@ -0,0 +1,22 @@
+package pieces;
+
+import java.awt.Point;
+
+import architecture.Drawable;
+import architecture.InteractionResult;
+import architecture.Player;
+
+public class SkeletonKing extends GamePiece {
+
+	public SkeletonKing(char symbol, Point location) {
+		super(symbol, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[][] pieces, Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
