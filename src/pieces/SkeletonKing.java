e27c3982737933cc8e4bc19ab2a00094abfeb4c5
Adam Nelson <adamnelson@mines.edu>
2017-10-27 11:46:51 -0600

    Began work on Skeleton King class.

--- src/pieces/SkeletonKing.java
+++ src/pieces/SkeletonKing.java
@@ -8,15 +8,36 @@ import architecture.Player;
 
 public class SkeletonKing extends GamePiece {
 
+	/*
+	 * Motion: None
+	 * 
+	 * Interaction: NONE interaction until a timer runs out. If the player
+	 * fails to beat the level in time, they die with KILL.
+	 */
+
+	private static int moveCounter; //Tracks the number of moves the player has left to beat the level
+
 	public SkeletonKing(char symbol, Point location) {
 		super(symbol, location);
-		// TODO Auto-generated constructor stub
+		moveCounter = 30; //Starting number of moves the player has to beat the level
+		System.out.println("\nThe Skeleton King has raised minions from the dead to");
+		System.out.println("terrorize the city! You must slay him before he gets");
+		System.out.println("too strong and can't be stopped!");
 	}
 
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Player player) {
-		// TODO Auto-generated method stub
-		return null;
+		//TODO Complete method
+		if (moveCounter == 0) {
+			System.out.println("\nYou are too late! The Skeleton King has too much dark");
+			System.out.println("energy to be destoyed! He opens his mouth and a stream");
+			System.out.println("of darkness surrounds you...");
+			return InteractionResult.KILL;
+		}
+		System.out.println("\nYou have " + moveCounter + " turns left to defeat the");
+		System.out.println("Skeleton King.");
+		moveCounter--;
+		return InteractionResult.NONE;
 	}
 
 }
