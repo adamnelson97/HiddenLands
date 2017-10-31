84033a1735d167583aac8222e3577dd63c49606b
Adam Nelson <[adamnelson@mines.edu]>
2017-10-31 11:38:26 -0600

    Added blocks to clear path and end level when all Skeleton Minions are dead.

--- src/pieces/SkeletonKing.java
+++ src/pieces/SkeletonKing.java
@@ -16,10 +16,14 @@ public class SkeletonKing extends GamePiece {
 	 */
 
 	private static int moveCounter; //Tracks the number of moves the player has left to beat the level
+	private static boolean allDead; //Tracks whether all the Skeleton minions are dead
+	private static boolean pathClear; //Tracks whether the path to the end space is clear or not
 
 	public SkeletonKing(char symbol, Point location) {
 		super(symbol, location);
 		moveCounter = 30; //Starting number of moves the player has to beat the level
+		allDead = false;
+		pathClear = false;
 		System.out.println("\nThe Skeleton King has raised minions from the dead to");
 		System.out.println("terrorize the city! You must slay him before he gets");
 		System.out.println("too strong and can't be stopped!");
@@ -28,6 +32,21 @@ public class SkeletonKing extends GamePiece {
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Player player) {
 		//TODO Complete method
+		if (allDead && !pathClear) {
+			pieces[1][7] = null; //Removes the rock blocking the path to end space
+			System.out.println("\nA path has appeared! Go to the top of the cliff");
+			System.out.println("and shoot the Skeleton King with your bow!");
+			pathClear = true; //Ensures this block only runs once
+		}
+		if (allDead) {
+			Point playerLocation = player.getLocation();
+			if (playerLocation.getX() == 1 && playerLocation.getY() == 7) {
+				System.out.println("\nSummiting the cliff, you draw back your bow.");
+				System.out.println("With your last arrow, you shoot the weakened");
+				System.out.println("Skeleton King and destroy him!");
+				return InteractionResult.ADVANCE; //Ends level
+			}
+		}
 		if (moveCounter == 0) {
 			System.out.println("\nYou are too late! The Skeleton King has too much dark");
 			System.out.println("energy to be destoyed! He opens his mouth and a stream");
