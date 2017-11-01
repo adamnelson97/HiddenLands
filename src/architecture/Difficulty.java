2916f5e458a9d42fcb99aca1d00fbf4b65fb2eec
Adam Nelson <adamnelson@mines.edu>
2017-11-01 17:43:09 -0600

    Added javadoc comments.

--- src/architecture/Difficulty.java
+++ src/architecture/Difficulty.java
@@ -8,6 +8,15 @@ package architecture;
  * HARDCORE: Four Levels, one hit point, two Boss Battles
  */
 
+/**
+ * <h1>Difficulty Enum</h1>
+ * The difficulty tracks how many levels the player must complete
+ * to 'beat' the game.
+ * @author Adam Nelson
+ * @version 1.0
+ * @since 2017-10-31
+ *
+ */
 public enum Difficulty {
 	EASY, MEDIUM, HARD, HARDCORE;
 }
