88e5c0b874979dd9baab94600674c022e1011df7
Adam Nelson <adamnelson@mines.edu>
2017-11-01 17:38:24 -0600

    Added javadoc comments to the Witch class.

--- src/pieces/Witch.java
+++ src/pieces/Witch.java
@@ -6,6 +6,15 @@ import architecture.Drawable;
 import architecture.InteractionResult;
 import architecture.Player;
 
+/**
+ * <h1>Witch</h1>
+ * The Witch is a beautiful but terrifying enchantress that kills
+ * players on sight!
+ * @author Adam Nelson
+ * @version 1.0
+ * @since 2017-10-31
+ *
+ */
 public class Witch extends GamePiece {
 
 	/*
@@ -16,11 +25,21 @@ public class Witch extends GamePiece {
 	 * Interaction: KILL. Has range of +/- 1 space from current space. Gives a warning from 2 spaces away.
 	 * */
 
-	//Constructor
+	/**
+	 * Default GamePiece Constructor.
+	 * @param symbol The symbol of the object on the board.
+	 * @param location The location of the object on the board.
+	 */
 	public Witch(char symbol, Point location) {
 		super(symbol, location);
 	}
 
+	/**
+	 * Kills the player if they are within one space in either direction of them.
+	 * @param pieces The game board.
+	 * @param player The player object.
+	 * @return InteractionResult KILL if within range, otherwise NONE.
+	 */
 	@Override
 	public InteractionResult interact(Drawable[][] pieces, Player player) {
 		Point playerLocation = player.getLocation();
@@ -38,10 +57,8 @@ public class Witch extends GamePiece {
 		}
 		else if (playerX <= witchX + warnDistance && playerX  >= witchX - warnDistance && playerY <= witchY + warnDistance && playerY >= witchY - warnDistance) {
 			System.out.println("\nYou sense an evil prescence ahead. Best to find another route!");
-			return InteractionResult.NONE;
 		}
-		else {
-			return InteractionResult.NONE;
-		}	}
+		return InteractionResult.NONE;
+	}
 
 }
