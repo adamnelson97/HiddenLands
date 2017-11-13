9e8c4b948a8729bb513753c0fe4d18c07028c63f
Adam Nelson <adamnelson@mines.edu>
2017-11-12 21:48:29 -0700

    Removed old imports.

--- src/architecture/Player.java
+++ src/architecture/Player.java
@@ -1,10 +1,6 @@
 package architecture;
 
 import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import pieces.Landscape;
